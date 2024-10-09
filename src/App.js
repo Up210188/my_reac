74bf6c6d96ea0334e85ba5796d63de3e6e482db6
Your Name <you@example.com>
2024-10-09 15:34:10 -0600

    hh

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          osvaldo esparza
         </a>
       </header>
     </div>
